c5c94044c2a488d2707c6bfee9afe82c0cc90a3b
dlebansais <dlebansais@gmail.com>
2022-11-23 23:01:59 +0100

    Updated ClassModel.

--- MakeConst/MakeConst.Test/ClassModeltUnitTests.cs
+++ MakeConst/MakeConst.Test/ClassModeltUnitTests.cs
@@ -8,6 +8,18 @@ using VerifyCS = CSharpCodeFixVerifier<ClassModelAnalyzer, ClassModelCodeFixProv
 public class ClassModeltUnitTests
 {
     [TestMethod]
+    public async Task ClassWithNoMembers_NoDiagnostic()
+    {
+        await VerifyCS.VerifyAnalyzerAsync(@"
+using System;
+
+class Program
+{
+}
+");
+    }
+
+    [TestMethod]
     public async Task ClassCouldHaveAModel_Diagnostic()
     {
         await VerifyCS.VerifyCodeFixAsync(@"
@@ -17,8 +29,6 @@ class [|Program|]
 {
     static void Main()
     {
-        const int i = 0;
-        Console.WriteLine(i);
     }
 }
 ", @"
@@ -29,8 +39,6 @@ class Program
 {
     static void Main()
     {
-        const int i = 0;
-        Console.WriteLine(i);
     }
 }
 ");
@@ -47,8 +55,6 @@ class Program
 {
     static void Main()
     {
-        int i = 0;
-        Console.WriteLine(i++);
     }
 }
 ");
@@ -66,8 +72,6 @@ class Program
 {
     static void Main()
     {
-        int i = 0;
-        Console.WriteLine(i++);
     }
 }
 ");
@@ -85,8 +89,6 @@ class Program
 {
     static void Main()
     {
-        int i = 0;
-        Console.WriteLine(i++);
     }
 }
 ");
@@ -103,8 +105,6 @@ class [|Program|]
 {
     static void Main()
     {
-        const int i = 0;
-        Console.WriteLine(i);
     }
 }
 ", @"
@@ -116,20 +116,111 @@ class Program
 {
     static void Main()
     {
-        const int i = 0;
-        Console.WriteLine(i);
     }
 }
 ");
     }
 
     [TestMethod]
-    public async Task ClassWithNoMembers_NoDiagnostic()
+    public async Task ClassWithAttributeCouldHaveAModel_Diagnostic()
     {
-        await VerifyCS.VerifyAnalyzerAsync(@"
+        await VerifyCS.VerifyCodeFixAsync(@"
 using System;
 
+[Serializable]
+class [|Program|]
+{
+}
+", @"
+using System;
+
+// No model
+[Serializable]
 class Program
+{
+}
+");
+    }
+
+    [TestMethod]
+    public async Task ClassWithUnsupportedModifierCouldHaveAModel_Diagnostic()
+    {
+        await VerifyCS.VerifyCodeFixAsync(@"
+using System;
+
+static class [|Program|]
+{
+}
+", @"
+using System;
+
+// No model
+static class Program
+{
+}
+");
+    }
+
+    [TestMethod]
+    public async Task ClassWithBaseTypeCouldHaveAModel_Diagnostic()
+    {
+        await VerifyCS.VerifyCodeFixAsync(@"
+using System;
+
+class [|Program|] : IDisposable
+{
+    public void Dispose()
+    {
+    }
+}
+", @"
+using System;
+
+// No model
+class Program : IDisposable
+{
+    public void Dispose()
+    {
+    }
+}
+");
+    }
+
+    [TestMethod]
+    public async Task ClassWithTypeParameterCouldHaveAModel_Diagnostic()
+    {
+        await VerifyCS.VerifyCodeFixAsync(@"
+using System;
+
+class [|Program|]<T>
+{
+}
+", @"
+using System;
+
+// No model
+class Program<T>
+{
+}
+");
+    }
+
+    [TestMethod]
+    public async Task ClassWithTypeConstraintCouldHaveAModel_Diagnostic()
+    {
+        await VerifyCS.VerifyCodeFixAsync(@"
+using System;
+
+class [|Program|]<T>
+where T : class
+{
+}
+", @"
+using System;
+
+// No model
+class Program<T>
+where T : class
 {
 }
 ");
@@ -158,6 +249,431 @@ class Program
 {
     int X;
     int Y;
+}
+");
+    }
+
+    [TestMethod]
+    public async Task ClassWithFieldAttributeCouldHaveAModel_Diagnostic()
+    {
+        await VerifyCS.VerifyCodeFixAsync(@"
+using System;
+
+class [|Program|]
+{
+    [ThreadStatic]
+    int X;
+}
+", @"
+using System;
+
+// No model
+class Program
+{
+    [ThreadStatic]
+    int X;
+}
+");
+    }
+
+    [TestMethod]
+    public async Task ClassWithSupportedFieldModifier_NoDiagnostic()
+    {
+        await VerifyCS.VerifyAnalyzerAsync(@"
+using System;
+
+class Program
+{
+    private int X;
+}
+");
+    }
+
+    [TestMethod]
+    public async Task ClassWithUnsupportedFieldModifierCouldHaveAModel_Diagnostic()
+    {
+        await VerifyCS.VerifyCodeFixAsync(@"
+using System;
+
+class [|Program|]
+{
+    static int X;
+}
+", @"
+using System;
+
+// No model
+class Program
+{
+    static int X;
+}
+");
+    }
+
+    [TestMethod]
+    public async Task ClassWithPublicFielCouldHaveAModel_Diagnostic()
+    {
+        await VerifyCS.VerifyCodeFixAsync(@"
+using System;
+
+class [|Program|]
+{
+    public int X;
+}
+", @"
+using System;
+
+// No model
+class Program
+{
+    public int X;
+}
+");
+    }
+
+    [TestMethod]
+    public async Task ClassWithUnsupportedFieldTypeCouldHaveAModel_Diagnostic()
+    {
+        await VerifyCS.VerifyCodeFixAsync(@"
+using System;
+
+class [|Program|]
+{
+    Program X;
+}
+", @"
+using System;
+
+// No model
+class Program
+{
+    Program X;
+}
+");
+    }
+
+    [TestMethod]
+    public async Task ClassWithUnsupportedFieldPredefinedTypeCouldHaveAModel_Diagnostic()
+    {
+        await VerifyCS.VerifyCodeFixAsync(@"
+using System;
+
+class [|Program|]
+{
+    string X;
+}
+", @"
+using System;
+
+// No model
+class Program
+{
+    string X;
+}
+");
+    }
+
+    [TestMethod]
+    public async Task ClassWithMethodMemberNoParameters_NoDiagnostic()
+    {
+        await VerifyCS.VerifyAnalyzerAsync(@"
+using System;
+
+class Program
+{
+    int Read()
+    {
+        return 0;
+    }
+}
+");
+    }
+
+    [TestMethod]
+    public async Task ClassWithMethodMemberWithNoReturn_NoDiagnostic()
+    {
+        await VerifyCS.VerifyAnalyzerAsync(@"
+using System;
+
+class Program
+{
+    void Write()
+    {
+    }
+}
+");
+    }
+
+    [TestMethod]
+    public async Task ClassWithPrivateMethodMember_NoDiagnostic()
+    {
+        await VerifyCS.VerifyAnalyzerAsync(@"
+using System;
+
+class Program
+{
+    private int Read()
+    {
+        return 0;
+    }
+}
+");
+    }
+
+    [TestMethod]
+    public async Task ClassWithPublicMethodMember_NoDiagnostic()
+    {
+        await VerifyCS.VerifyAnalyzerAsync(@"
+using System;
+
+class Program
+{
+    public int Read()
+    {
+        return 0;
+    }
+}
+");
+    }
+
+    [TestMethod]
+    public async Task ClassWithMethodAttributeCouldHaveAModel_Diagnostic()
+    {
+        await VerifyCS.VerifyCodeFixAsync(@"
+using System;
+
+class [|Program|]
+{
+    [MTAThread]
+    int Read()
+    {
+        return 0;
+    }
+}
+", @"
+using System;
+
+// No model
+class Program
+{
+    [MTAThread]
+    int Read()
+    {
+        return 0;
+    }
+}
+");
+    }
+
+    [TestMethod]
+    public async Task ClassWithUnsupportedMethodModifierCouldHaveAModel_Diagnostic()
+    {
+        await VerifyCS.VerifyCodeFixAsync(@"
+using System;
+
+class [|Program|]
+{
+    static int Read()
+    {
+        return 0;
+    }
+}
+", @"
+using System;
+
+// No model
+class Program
+{
+    static int Read()
+    {
+        return 0;
+    }
+}
+");
+    }
+
+    [TestMethod]
+    public async Task ClassWithUnsupportedReturnTypeCouldHaveAModel_Diagnostic()
+    {
+        await VerifyCS.VerifyCodeFixAsync(@"
+using System;
+
+class [|Program|]
+{
+    Program Read()
+    {
+        return null!;
+    }
+}
+", @"
+using System;
+
+// No model
+class Program
+{
+    Program Read()
+    {
+        return null!;
+    }
+}
+");
+    }
+
+    [TestMethod]
+    public async Task ClassWithUnsupportedReturnPredefinedTypeCouldHaveAModel_Diagnostic()
+    {
+        await VerifyCS.VerifyCodeFixAsync(@"
+using System;
+
+class [|Program|]
+{
+    string Read()
+    {
+        return null!;
+    }
+}
+", @"
+using System;
+
+// No model
+class Program
+{
+    string Read()
+    {
+        return null!;
+    }
+}
+");
+    }
+
+    [TestMethod]
+    public async Task ClassWithMethodMemberWithOneParameter_NoDiagnostic()
+    {
+        await VerifyCS.VerifyAnalyzerAsync(@"
+using System;
+
+class Program
+{
+    int Read(int x)
+    {
+        return 0;
+    }
+}
+");
+    }
+
+    [TestMethod]
+    public async Task ClassWithMethodMemberWithTwoParameters_NoDiagnostic()
+    {
+        await VerifyCS.VerifyAnalyzerAsync(@"
+using System;
+
+class Program
+{
+    int Read(int x, int y)
+    {
+        return 0;
+    }
+}
+");
+    }
+
+    [TestMethod]
+    public async Task ClassWithParameterAttributeCouldHaveAModel_Diagnostic()
+    {
+        await VerifyCS.VerifyCodeFixAsync(@"
+using System;
+
+class [|Program|]
+{
+    void Write([System.Runtime.InteropServices.In] int x)
+    {
+    }
+}
+", @"
+using System;
+
+// No model
+class Program
+{
+    void Write([System.Runtime.InteropServices.In] int x)
+    {
+    }
+}
+");
+    }
+
+    [TestMethod]
+    public async Task ClassWithParameterModifierCouldHaveAModel_Diagnostic()
+    {
+        await VerifyCS.VerifyCodeFixAsync(@"
+using System;
+
+class [|Program|]
+{
+    void Write(ref int x)
+    {
+    }
+}
+", @"
+using System;
+
+// No model
+class Program
+{
+    void Write(ref int x)
+    {
+    }
+}
+");
+    }
+
+    [TestMethod]
+    public async Task ClassWithUnsupportedParameterTypeCouldHaveAModel_Diagnostic()
+    {
+        await VerifyCS.VerifyCodeFixAsync(@"
+using System;
+
+class [|Program|]
+{
+    void Write(Program p)
+    {
+    }
+}
+", @"
+using System;
+
+// No model
+class Program
+{
+    void Write(Program p)
+    {
+    }
+}
+");
+    }
+
+    [TestMethod]
+    public async Task ClassWithUnsupportedParameterPredefinedTypeCouldHaveAModel_Diagnostic()
+    {
+        await VerifyCS.VerifyCodeFixAsync(@"
+using System;
+
+class [|Program|]
+{
+    void Write(string p)
+    {
+    }
+}
+", @"
+using System;
+
+// No model
+class Program
+{
+    void Write(string p)
+    {
+    }
 }
 ");
     }
--- MakeConst/MakeConst/ClassModelAnalyzer.cs
+++ MakeConst/MakeConst/ClassModelAnalyzer.cs
@@ -1,8 +1,7 @@
 ﻿namespace DemoAnalyzer;
 
-using System.Collections.Generic;
 using System.Collections.Immutable;
-using System.Xml;
+using System.Xml.Linq;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
@@ -34,34 +33,148 @@ public class ClassModelAnalyzer : DiagnosticAnalyzer
         var classDeclaration = (ClassDeclarationSyntax)context.Node;
 
         // Check whether the class can be modeled
+        if (IsClassIgnoredForModeling(classDeclaration))
+            return;
+
+        if (IsClassDeclarationSupported(classDeclaration) && AreAllMembersSupported(classDeclaration))
+            return;
+
+        context.ReportDiagnostic(Diagnostic.Create(Rule, classDeclaration.Identifier.GetLocation(), classDeclaration.Identifier.ValueText));
+    }
+
+    private static bool IsClassIgnoredForModeling(ClassDeclarationSyntax classDeclaration)
+    {
         SyntaxToken firstToken = classDeclaration.GetFirstToken();
         SyntaxTriviaList leadingTrivia = firstToken.LeadingTrivia;
+
         foreach (SyntaxTrivia Trivia in leadingTrivia)
             if (Trivia.Kind() == SyntaxKind.SingleLineCommentTrivia)
             {
                 string Comment = Trivia.ToFullString();
                 if (Comment.StartsWith($"// {Modeling.None}"))
-                    return;
+                    return true;
             }
 
-        List<FieldDeclarationSyntax> Fields = new();
-        bool HasUnsupported = false;
+        return false;
+    }
+
+    private static bool IsClassDeclarationSupported(ClassDeclarationSyntax classDeclaration)
+    {
+        if (classDeclaration.AttributeLists.Count > 0)
+            return false;
+
+        foreach (SyntaxToken Modifier in classDeclaration.Modifiers)
+        {
+            SyntaxKind Kind = Modifier.Kind();
+            if (Kind != SyntaxKind.PrivateKeyword && Kind != SyntaxKind.PublicKeyword && Kind != SyntaxKind.PartialKeyword)
+                return false;
+        }
+
+        if (classDeclaration.BaseList is BaseListSyntax BaseList && BaseList.Types.Count > 0)
+            return false;
+
+        if (classDeclaration.TypeParameterList is TypeParameterListSyntax TypeParameterList && TypeParameterList.Parameters.Count > 0)
+            return false;
+
+        if (classDeclaration.ConstraintClauses.Count > 0)
+            return false;
+
+        return true;
+    }
+
+    private static bool AreAllMembersSupported(ClassDeclarationSyntax classDeclaration)
+    {
         foreach (MemberDeclarationSyntax Member in classDeclaration.Members)
+            if (!IsMemberSupported(Member))
+                return false;
+
+        return true;
+    }
+
+    private static bool IsMemberSupported(MemberDeclarationSyntax member)
+    {
+        switch (member)
         {
-            switch (Member)
-            {
-                case FieldDeclarationSyntax AsField:
-                    Fields.Add(AsField);
-                    break;
-                default:
-                    HasUnsupported = true;
-                    break;
-            }
+            case FieldDeclarationSyntax AsField:
+                return IsFieldSupported(AsField);
+            case MethodDeclarationSyntax AsMethod:
+                return IsMethodSupported(AsMethod);
+            default:
+                return false;
+        }
+    }
+
+    private static bool IsFieldSupported(FieldDeclarationSyntax field)
+    {
+        if (field.AttributeLists.Count > 0)
+            return false;
+
+        foreach (SyntaxToken Modifier in field.Modifiers)
+        {
+            SyntaxKind Kind = Modifier.Kind();
+            if (Kind != SyntaxKind.PrivateKeyword)
+                return false;
         }
 
-        if (HasUnsupported)
+        if (!IsTypeSupported(field.Declaration.Type))
+            return false;
+
+        return true;
+    }
+
+    private static bool IsMethodSupported(MethodDeclarationSyntax method)
+    {
+        if (method.AttributeLists.Count > 0)
+            return false;
+
+        foreach (SyntaxToken Modifier in method.Modifiers)
         {
-            context.ReportDiagnostic(Diagnostic.Create(Rule, classDeclaration.Identifier.GetLocation(), classDeclaration.Identifier.ValueText));
+            SyntaxKind Kind = Modifier.Kind();
+            if (Kind != SyntaxKind.PrivateKeyword && Kind != SyntaxKind.PublicKeyword)
+                return false;
         }
+
+        if (!IsTypeSupported(method.ReturnType))
+            return false;
+
+        if (!AreAllMethodParametersSupported(method))
+            return false;
+
+        return true;
+    }
+
+    private static bool AreAllMethodParametersSupported(MethodDeclarationSyntax method)
+    {
+        foreach (ParameterSyntax Parameter in method.ParameterList.Parameters)
+            if (!IsParameterSupported(Parameter))
+                return false;
+
+        return true;
+    }
+
+    private static bool IsParameterSupported(ParameterSyntax parameter)
+    {
+        if (parameter.AttributeLists.Count > 0)
+            return false;
+
+        if (parameter.Modifiers.Count > 0)
+            return false;
+
+        if (!IsTypeSupported(parameter.Type))
+            return false;
+
+        return true;
+    }
+
+    private static bool IsTypeSupported(TypeSyntax type)
+    {
+        if (type is not PredefinedTypeSyntax AsPredefinedType)
+            return false;
+
+        SyntaxKind TypeKind = AsPredefinedType.Keyword.Kind();
+        if (TypeKind != SyntaxKind.IntKeyword && TypeKind != SyntaxKind.VoidKeyword)
+            return false;
+
+        return true;
     }
 }
