e49345e29f0c9fa34e68e9961305d01a9d6eeee0
dlebansais <dlebansais@gmail.com>
2022-11-24 19:03:11 +0100

    Moved to C# 11.

---
+++ MakeConst/MakeConst/ClassModel/ClassModel.cs
@@ -0,0 +1,8 @@
+﻿namespace DemoAnalyzer;
+
+using System.Collections.Generic;
+
+public class ClassModel
+{
+    public List<Identifier> FieldIdentifierList { get; set; } = new();
+}
---
+++ MakeConst/MakeConst/ClassModel/Identifier.cs
@@ -0,0 +1,6 @@
+﻿namespace DemoAnalyzer;
+
+public class Identifier
+{
+    public required string Name { get; set; }
+}
