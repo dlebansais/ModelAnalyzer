4ddc45cfc1794d4883aa89095cfd1fa56cc4a95e
dlebansais <dlebansais@gmail.com>
2022-12-02 19:44:52 +0100

    New solution.

--- ModelAnalyzer.Vsix/copyfiles.bat
+++ ModelAnalyzer.Vsix/copyfiles.bat
@@ -2,6 +2,7 @@ rem %1 is the destination path but with some file name added
 rem %2 is the solution name
 rem %3 is the source path for z3 library files
 rem %4 is the source manifest
+rem %5 is the destination manifest
 
 setlocal
 set INPUT_FOLDER_WITH_BACKSLASH=%~dp1
@@ -14,6 +15,11 @@ set SOURCE_FOLDER=%3
 rem echo Input: %SOURCE_FOLDER%
 rem echo File: %4
 
+if not exist %TARGET_FOLDER% goto end
+
+echo Copying files to: %TARGET_FOLDER%
 copy /Y %SOURCE_FOLDER%\libz3.dll %TARGET_FOLDER%
 copy /Y %SOURCE_FOLDER%\Microsoft.Z3.dll %TARGET_FOLDER%
-copy /Y %4 %TARGET_FOLDER%
+copy /Y %4 %TARGET_FOLDER%\%5
+
+:end
