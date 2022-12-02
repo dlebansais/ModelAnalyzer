446502b1e909fff62860bf511a9a735083d05883
dlebansais <dlebansais@gmail.com>
2022-12-02 11:24:58 +0100

    Refactoring.

---
+++ MakeConst/MakeConst/ClassModel/Verification/Verifier.Expression.cs
@@ -0,0 +1,71 @@
+﻿namespace DemoAnalyzer;
+
+using Microsoft.Z3;
+using System;
+
+public partial class Verifier : IDisposable
+{
+    private T BuildExpression<T>(AliasTable aliasTable, IExpression expression)
+        where T : Expr
+    {
+        Expr Result;
+
+        switch (expression)
+        {
+            case BinaryArithmeticExpression BinaryArithmetic:
+                Result = BuildBinaryExpression(aliasTable, BinaryArithmetic);
+                break;
+            case BinaryLogicalExpression BinaryLogical:
+                Result = BuildBinaryLogicalExpression(aliasTable, BinaryLogical);
+                break;
+            case ComparisonExpression Comparison:
+                Result = BuildComparisonExpression(aliasTable, Comparison);
+                break;
+            case LiteralValueExpression LiteralValue:
+                Result = BuildLiteralValueExpression(LiteralValue);
+                break;
+            case VariableValueExpression VariableValue:
+                Result = BuildVariableValueExpression(aliasTable, VariableValue);
+                break;
+            case UnsupportedExpression:
+            default:
+                throw new InvalidOperationException("Unexpected unsupported expression");
+        }
+
+        return Result as T ?? throw new InvalidOperationException($"Expected expression of type {typeof(T).Name}");
+    }
+
+    private ArithExpr BuildBinaryExpression(AliasTable aliasTable, BinaryArithmeticExpression binaryArithmeticExpression)
+    {
+        ArithExpr Left = BuildExpression<ArithExpr>(aliasTable, binaryArithmeticExpression.Left);
+        ArithExpr Right = BuildExpression<ArithExpr>(aliasTable, binaryArithmeticExpression.Right);
+        return ClassModel.SupportedArithmeticOperators[binaryArithmeticExpression.OperatorKind].Asserter(ctx, Left, Right);
+    }
+
+    private BoolExpr BuildBinaryLogicalExpression(AliasTable aliasTable, BinaryLogicalExpression binaryLogicalExpression)
+    {
+        BoolExpr Left = BuildExpression<BoolExpr>(aliasTable, binaryLogicalExpression.Left);
+        BoolExpr Right = BuildExpression<BoolExpr>(aliasTable, binaryLogicalExpression.Right);
+        return ClassModel.SupportedLogicalOperators[binaryLogicalExpression.OperatorKind].Asserter(ctx, Left, Right);
+    }
+
+    private BoolExpr BuildComparisonExpression(AliasTable aliasTable, ComparisonExpression comparisonExpression)
+    {
+        ArithExpr Left = BuildExpression<ArithExpr>(aliasTable, comparisonExpression.Left);
+        ArithExpr Right = BuildExpression<ArithExpr>(aliasTable, comparisonExpression.Right);
+        return ClassModel.SupportedComparisonOperators[comparisonExpression.OperatorKind].Asserter(ctx, Left, Right);
+    }
+
+    private ArithExpr BuildLiteralValueExpression(LiteralValueExpression literalValueExpression)
+    {
+        return ctx.MkInt(literalValueExpression.Value);
+    }
+
+    private ArithExpr BuildVariableValueExpression(AliasTable aliasTable, VariableValueExpression variableValueExpression)
+    {
+        string VariableName = variableValueExpression.Variable.Name;
+        string VariableAliasName = aliasTable.GetAlias(VariableName);
+
+        return ctx.MkIntConst(VariableAliasName);
+    }
+}
---
+++ MakeConst/MakeConst/ClassModel/Verification/Verifier.Statement.cs
@@ -0,0 +1,96 @@
+﻿namespace DemoAnalyzer;
+
+using Microsoft.Z3;
+using System;
+using System.Collections.Generic;
+
+public partial class Verifier : IDisposable
+{
+    private void AddStatementListExecution(Solver solver, AliasTable aliasTable, BoolExpr branch, List<IStatement> statementList)
+    {
+        foreach (IStatement Statement in statementList)
+            switch (Statement)
+            {
+                case AssignmentStatement Assignment:
+                    AddAssignmentExecution(solver, aliasTable, branch, Assignment);
+                    break;
+                case ConditionalStatement Conditional:
+                    AddConditionalExecution(solver, aliasTable, branch, Conditional);
+                    break;
+                case ReturnStatement Return:
+                    AddReturnExecution(solver, aliasTable, branch, Return);
+                    break;
+                case UnsupportedStatement:
+                default:
+                    throw new InvalidOperationException("Unexpected unsupported statement");
+            }
+    }
+
+    private void AddAssignmentExecution(Solver solver, AliasTable aliasTable, BoolExpr branch, AssignmentStatement assignmentStatement)
+    {
+        Expr SourceExpr = BuildExpression<Expr>(aliasTable, assignmentStatement.Expression);
+
+        string DestinationName = assignmentStatement.Destination.Name;
+        aliasTable.IncrementNameAlias(DestinationName);
+        string DestinationNameAlias = aliasTable.GetAlias(DestinationName);
+        IntExpr DestinationExpr = ctx.MkIntConst(DestinationNameAlias);
+
+        AddToSolver(solver, branch, ctx.MkEq(DestinationExpr, SourceExpr));
+    }
+
+    private void AddConditionalExecution(Solver solver, AliasTable aliasTable, BoolExpr branch, ConditionalStatement conditionalStatement)
+    {
+        BoolExpr ConditionExpr = BuildExpression<BoolExpr>(aliasTable, conditionalStatement.Condition);
+        BoolExpr TrueBranchExpr = ctx.MkAnd(branch, ConditionExpr);
+        BoolExpr FalseBranchExpr = ctx.MkAnd(branch, ctx.MkNot(ConditionExpr));
+
+        AliasTable BeforeWhenTrue = aliasTable.Clone();
+        AddStatementListExecution(solver, aliasTable, TrueBranchExpr, conditionalStatement.WhenTrueStatementList);
+        List<string> AliasesOnlyWhenTrue = aliasTable.GetAliasDifference(BeforeWhenTrue);
+
+        AliasTable WhenTrueAliasTable = aliasTable.Clone();
+
+        AliasTable BeforeWhenFalse = WhenTrueAliasTable;
+        AddStatementListExecution(solver, aliasTable, FalseBranchExpr, conditionalStatement.WhenFalseStatementList);
+        List<string> AliasesOnlyWhenFalse = aliasTable.GetAliasDifference(BeforeWhenFalse);
+
+        AliasTable WhenFalseAliasTable = aliasTable.Clone();
+
+        aliasTable.Merge(WhenTrueAliasTable, out List<string> UpdatedNameList);
+
+        foreach (string NameAlias in AliasesOnlyWhenFalse)
+        {
+            IntExpr AliasExpr = ctx.MkIntConst(NameAlias);
+            BoolExpr InitExpr = ctx.MkEq(AliasExpr, Zero);
+            AddToSolver(solver, TrueBranchExpr, InitExpr);
+        }
+
+        foreach (string NameAlias in AliasesOnlyWhenTrue)
+        {
+            IntExpr AliasExpr = ctx.MkIntConst(NameAlias);
+            BoolExpr InitExpr = ctx.MkEq(AliasExpr, Zero);
+            AddToSolver(solver, FalseBranchExpr, InitExpr);
+        }
+
+        foreach (string Name in UpdatedNameList)
+        {
+            string NameAlias = aliasTable.GetAlias(Name);
+            IntExpr DestinationExpr = ctx.MkIntConst(NameAlias);
+
+            string WhenTrueNameAlias = WhenTrueAliasTable.GetAlias(Name);
+            IntExpr WhenTrueSourceExpr = ctx.MkIntConst(WhenTrueNameAlias);
+            BoolExpr WhenTrueInitExpr = ctx.MkEq(DestinationExpr, WhenTrueSourceExpr);
+
+            string WhenFalseNameAlias = WhenFalseAliasTable.GetAlias(Name);
+            IntExpr WhenFalseSourceExpr = ctx.MkIntConst(WhenFalseNameAlias);
+            BoolExpr WhenFalseInitExpr = ctx.MkEq(DestinationExpr, WhenFalseSourceExpr);
+
+            AddToSolver(solver, TrueBranchExpr, WhenTrueInitExpr);
+            AddToSolver(solver, FalseBranchExpr, WhenFalseInitExpr);
+        }
+    }
+
+    private void AddReturnExecution(Solver solver, AliasTable aliasTable, BoolExpr branch, ReturnStatement returnStatement)
+    {
+    }
+}
