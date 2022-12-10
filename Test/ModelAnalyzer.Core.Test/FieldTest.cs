d80dfb42ced1b2a5ae48f75e089e01f92bbfa565
dlebansais <dlebansais@gmail.com>
2022-12-10 17:35:09 +0100

    Added tests.

--- Test/ModelAnalyzer.Core.Test/FieldTest.cs
+++ Test/ModelAnalyzer.Core.Test/FieldTest.cs
@@ -57,4 +57,93 @@ class Program_CoreField_1
         Assert.That(ClassModelString, Is.EqualTo(@"Program_CoreField_1
 "));
     }
+
+    [Test]
+    [Category("Core")]
+    public void UnsupportedFieldTest_Attribute()
+    {
+        ClassDeclarationSyntax ClassDeclaration = TestHelper.FromSourceCode(@"
+using System;
+
+class Program_CoreField_2
+{
+    [Serializable]
+    int X;
+}
+");
+
+        using TokenReplacement TokenReplacement = TestHelper.BeginReplaceToken(ClassDeclaration);
+
+        IClassModel ClassModel = TestHelper.ToClassModel(ClassDeclaration, TokenReplacement);
+
+        Assert.That(ClassModel.Unsupported.IsEmpty, Is.False);
+        Assert.That(ClassModel.Unsupported.Fields.Count, Is.EqualTo(1));
+    }
+
+    [Test]
+    [Category("Core")]
+    public void UnsupportedFieldTest_Modifier()
+    {
+        ClassDeclarationSyntax ClassDeclaration = TestHelper.FromSourceCode(@"
+using System;
+
+class Program_CoreField_3
+{
+    public int X;
+}
+");
+
+        using TokenReplacement TokenReplacement = TestHelper.BeginReplaceToken(ClassDeclaration);
+
+        IClassModel ClassModel = TestHelper.ToClassModel(ClassDeclaration, TokenReplacement);
+
+        Assert.That(ClassModel.Unsupported.IsEmpty, Is.False);
+        Assert.That(ClassModel.Unsupported.Fields.Count, Is.EqualTo(1));
+    }
+
+    [Test]
+    [Category("Core")]
+    public void ModifierTest()
+    {
+        ClassDeclarationSyntax ClassDeclaration = TestHelper.FromSourceCode(@"
+using System;
+
+class Program_CoreField_4
+{
+    private int X;
+}
+");
+
+        using TokenReplacement TokenReplacement = TestHelper.BeginReplaceToken(ClassDeclaration);
+
+        IClassModel ClassModel = TestHelper.ToClassModel(ClassDeclaration, TokenReplacement);
+
+        Assert.That(ClassModel.Unsupported.IsEmpty, Is.True);
+
+        string? ClassModelString = ClassModel.ToString();
+        Assert.That(ClassModelString, Is.EqualTo(@"Program_CoreField_4
+  int X
+"));
+    }
+
+    [Test]
+    [Category("Core")]
+    public void DuplicateFieldNameTest()
+    {
+        ClassDeclarationSyntax ClassDeclaration = TestHelper.FromSourceCode(@"
+using System;
+
+class Program_CoreField_5
+{
+    int X;
+    int X;
+}
+");
+
+        using TokenReplacement TokenReplacement = TestHelper.BeginReplaceToken(ClassDeclaration);
+
+        IClassModel ClassModel = TestHelper.ToClassModel(ClassDeclaration, TokenReplacement);
+
+        Assert.That(ClassModel.Unsupported.IsEmpty, Is.True);
+    }
 }
