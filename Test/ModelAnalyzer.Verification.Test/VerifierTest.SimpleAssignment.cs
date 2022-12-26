919d3f296331e1de6da9397f55acd024d4a999a0
dlebansais <dlebansais@gmail.com>
2022-12-26 13:06:58 +0100

    Added some tests.

---
+++ Test/ModelAnalyzer.Verification.Test/VerifierTest.SimpleAssignment.cs
@@ -0,0 +1,182 @@
+﻿namespace ModelAnalyzer.Verification.Test;
+
+using System.Collections.Generic;
+using NUnit.Framework;
+
+/// <summary>
+/// Tests for the <see cref="Verifier"/> class.
+/// </summary>
+public partial class VerifierTest
+{
+    [Test]
+    [Category("Verification")]
+    public void Verifier_SimpleAssignment_OneBooleanFieldSuccess()
+    {
+        Verifier TestObject = CreateSimpleAssignmentVerifier<bool, LiteralBooleanValueExpression>(invariantTestValue: false, assignmentValue: true, maxDepth: 0);
+
+        TestObject.Verify();
+
+        VerificationResult VerificationResult = TestObject.VerificationResult;
+        Assert.That(VerificationResult.IsSuccess, Is.True);
+    }
+
+    [Test]
+    [Category("Verification")]
+    public void Verifier_SimpleAssignment_OneBooleanFieldError()
+    {
+        Verifier TestObject = CreateSimpleAssignmentVerifier<bool, LiteralBooleanValueExpression>(invariantTestValue: true, assignmentValue: true, maxDepth: 0);
+
+        TestObject.Verify();
+
+        VerificationResult VerificationResult = TestObject.VerificationResult;
+        Assert.That(VerificationResult.IsError, Is.True);
+        Assert.That(VerificationResult.ErrorType == VerificationErrorType.InvariantError);
+    }
+
+    [Test]
+    [Category("Verification")]
+    public void Verifier_SimpleAssignment_OneBooleanFieldWithWriteError()
+    {
+        Verifier TestObject = CreateSimpleAssignmentVerifier<bool, LiteralBooleanValueExpression>(invariantTestValue: false, assignmentValue: true, maxDepth: 1);
+
+        TestObject.Verify();
+
+        VerificationResult VerificationResult = TestObject.VerificationResult;
+        Assert.That(VerificationResult.IsError, Is.True);
+        Assert.That(VerificationResult.ErrorType == VerificationErrorType.InvariantError);
+    }
+
+    [Test]
+    [Category("Verification")]
+    public void Verifier_SimpleAssignment_OneIntegerFieldSuccess()
+    {
+        Verifier TestObject = CreateSimpleAssignmentVerifier<int, LiteralIntegerValueExpression>(invariantTestValue: 0, assignmentValue: 1, maxDepth: 0);
+
+        TestObject.Verify();
+
+        VerificationResult VerificationResult = TestObject.VerificationResult;
+        Assert.That(VerificationResult.IsSuccess, Is.True);
+    }
+
+    [Test]
+    [Category("Verification")]
+    public void Verifier_SimpleAssignment_OneIntegerFieldError()
+    {
+        Verifier TestObject = CreateSimpleAssignmentVerifier<int, LiteralIntegerValueExpression>(invariantTestValue: 1, assignmentValue: 1, maxDepth: 0);
+
+        TestObject.Verify();
+
+        VerificationResult VerificationResult = TestObject.VerificationResult;
+        Assert.That(VerificationResult.IsError, Is.True);
+        Assert.That(VerificationResult.ErrorType == VerificationErrorType.InvariantError);
+    }
+
+    [Test]
+    [Category("Verification")]
+    public void Verifier_SimpleAssignment_OneIntegerFieldWithWriteError()
+    {
+        Verifier TestObject = CreateSimpleAssignmentVerifier<int, LiteralIntegerValueExpression>(invariantTestValue: 0, assignmentValue: 1, maxDepth: 1);
+
+        TestObject.Verify();
+
+        VerificationResult VerificationResult = TestObject.VerificationResult;
+        Assert.That(VerificationResult.IsError, Is.True);
+        Assert.That(VerificationResult.ErrorType == VerificationErrorType.InvariantError);
+    }
+
+    [Test]
+    [Category("Verification")]
+    public void Verifier_SimpleAssignment_OneFloatingPointFieldSuccess()
+    {
+        Verifier TestObject = CreateSimpleAssignmentVerifier<double, LiteralFloatingPointValueExpression>(invariantTestValue: 0.0, assignmentValue: 1.0, maxDepth: 0);
+
+        TestObject.Verify();
+
+        VerificationResult VerificationResult = TestObject.VerificationResult;
+        Assert.That(VerificationResult.IsSuccess, Is.True);
+    }
+
+    [Test]
+    [Category("Verification")]
+    public void Verifier_SimpleAssignment_OneFloatingPointFieldError()
+    {
+        Verifier TestObject = CreateSimpleAssignmentVerifier<double, LiteralFloatingPointValueExpression>(invariantTestValue: 1.0, assignmentValue: 1.0, maxDepth: 0);
+
+        TestObject.Verify();
+
+        VerificationResult VerificationResult = TestObject.VerificationResult;
+        Assert.That(VerificationResult.IsError, Is.True);
+        Assert.That(VerificationResult.ErrorType == VerificationErrorType.InvariantError);
+    }
+
+    [Test]
+    [Category("Verification")]
+    public void Verifier_SimpleAssignment_OneFloatingPointFieldWithWriteError()
+    {
+        Verifier TestObject = CreateSimpleAssignmentVerifier<double, LiteralFloatingPointValueExpression>(invariantTestValue: 0.0, assignmentValue: 1.0, maxDepth: 1);
+
+        TestObject.Verify();
+
+        VerificationResult VerificationResult = TestObject.VerificationResult;
+        Assert.That(VerificationResult.IsError, Is.True);
+        Assert.That(VerificationResult.ErrorType == VerificationErrorType.InvariantError);
+    }
+
+    private Verifier CreateSimpleAssignmentVerifier<TValue, TExpression>(TValue invariantTestValue, TValue assignmentValue, int maxDepth)
+        where TExpression : Expression, ILiteralExpression<TValue>, ILiteralExpression, new()
+    {
+        TExpression Zero = new() { Value = invariantTestValue };
+        TExpression AssignmentSource = new() { Value = assignmentValue };
+
+        string ClassName = "Test";
+        string FieldName = "X";
+        Field TestField = new()
+        {
+            FieldName = new FieldName { Name = FieldName },
+            VariableType = Zero.ExpressionType,
+            Initializer = null,
+        };
+
+        FieldTable TestFieldTable = new();
+        TestFieldTable.AddItem(TestField.FieldName, TestField);
+
+        VariableValueExpression Variable = new() { Variable = TestField };
+        EqualityExpression VariableEqualZero = new EqualityExpression() { Left = Variable, Right = Zero, Operator = EqualityOperator.Equal };
+
+        Invariant TestInvariant = new()
+        {
+            BooleanExpression = VariableEqualZero,
+            Text = VariableEqualZero.ToString(),
+        };
+
+        List<Invariant> InvariantList = new() { TestInvariant };
+
+        AssignmentStatement Assignment = new() { Destination = TestField, Expression = AssignmentSource };
+
+        string TestMethodName = "Write";
+        MethodName MethodName = new() { Name = TestMethodName };
+        Method TestMethod = new()
+        {
+            MethodName = MethodName,
+            RequireList = new(),
+            ParameterTable = ReadOnlyParameterTable.Empty,
+            StatementList = new() { Assignment },
+            EnsureList = new(),
+            ReturnType = ExpressionType.Void,
+        };
+
+        MethodTable MethodTable = new();
+        MethodTable.AddItem(TestMethod.MethodName, TestMethod);
+
+        Verifier TestObject = new()
+        {
+            ClassName = ClassName,
+            FieldTable = TestFieldTable.ToReadOnly(),
+            MethodTable = MethodTable.ToReadOnly(),
+            InvariantList = InvariantList,
+            MaxDepth = maxDepth,
+        };
+
+        return TestObject;
+    }
+}
---
+++ Test/ModelAnalyzer.Verification.Test/VerifierTest.SimpleField.cs
@@ -0,0 +1,125 @@
+﻿namespace ModelAnalyzer.Verification.Test;
+
+using System.Collections.Generic;
+using NUnit.Framework;
+
+/// <summary>
+/// Tests for the <see cref="Verifier"/> class.
+/// </summary>
+public partial class VerifierTest
+{
+    [Test]
+    [Category("Verification")]
+    public void Verifier_OneBooleanFieldSuccess()
+    {
+        Verifier TestObject = CreateOneFieldVerifier<bool, LiteralBooleanValueExpression>(invariantTestValue: false);
+
+        TestObject.Verify();
+
+        VerificationResult VerificationResult = TestObject.VerificationResult;
+        Assert.That(VerificationResult.IsSuccess, Is.True);
+    }
+
+    [Test]
+    [Category("Verification")]
+    public void Verifier_OneBooleanFieldError()
+    {
+        Verifier TestObject = CreateOneFieldVerifier<bool, LiteralBooleanValueExpression>(invariantTestValue: true);
+
+        TestObject.Verify();
+
+        VerificationResult VerificationResult = TestObject.VerificationResult;
+        Assert.That(VerificationResult.IsError, Is.True);
+        Assert.That(VerificationResult.ErrorType == VerificationErrorType.InvariantError);
+    }
+
+    [Test]
+    [Category("Verification")]
+    public void Verifier_OneIntegerFieldSuccess()
+    {
+        Verifier TestObject = CreateOneFieldVerifier<int, LiteralIntegerValueExpression>(invariantTestValue: 0);
+
+        TestObject.Verify();
+
+        VerificationResult VerificationResult = TestObject.VerificationResult;
+        Assert.That(VerificationResult.IsSuccess, Is.True);
+    }
+
+    [Test]
+    [Category("Verification")]
+    public void Verifier_OneIntegerFieldError()
+    {
+        Verifier TestObject = CreateOneFieldVerifier<int, LiteralIntegerValueExpression>(invariantTestValue: 1);
+
+        TestObject.Verify();
+
+        VerificationResult VerificationResult = TestObject.VerificationResult;
+        Assert.That(VerificationResult.IsError, Is.True);
+        Assert.That(VerificationResult.ErrorType == VerificationErrorType.InvariantError);
+    }
+
+    [Test]
+    [Category("Verification")]
+    public void Verifier_OneFloatingPointFieldSuccess()
+    {
+        Verifier TestObject = CreateOneFieldVerifier<double, LiteralFloatingPointValueExpression>(invariantTestValue: 0);
+
+        TestObject.Verify();
+
+        VerificationResult VerificationResult = TestObject.VerificationResult;
+        Assert.That(VerificationResult.IsSuccess, Is.True);
+    }
+
+    [Test]
+    [Category("Verification")]
+    public void Verifier_OneFloatingPointFieldError()
+    {
+        Verifier TestObject = CreateOneFieldVerifier<double, LiteralFloatingPointValueExpression>(invariantTestValue: 1.0);
+
+        TestObject.Verify();
+
+        VerificationResult VerificationResult = TestObject.VerificationResult;
+        Assert.That(VerificationResult.IsError, Is.True);
+        Assert.That(VerificationResult.ErrorType == VerificationErrorType.InvariantError);
+    }
+
+    private Verifier CreateOneFieldVerifier<TValue, TExpression>(TValue invariantTestValue)
+        where TExpression : Expression, ILiteralExpression<TValue>, ILiteralExpression, new()
+    {
+        TExpression Zero = new() { Value = invariantTestValue };
+
+        string ClassName = "Test";
+        string FieldName = "X";
+        Field TestField = new()
+        {
+            FieldName = new FieldName { Name = FieldName },
+            VariableType = Zero.ExpressionType,
+            Initializer = null,
+        };
+
+        FieldTable TestFieldTable = new();
+        TestFieldTable.AddItem(TestField.FieldName, TestField);
+
+        VariableValueExpression Variable = new() { Variable = TestField };
+        EqualityExpression VariableEqualZero = new EqualityExpression() { Left = Variable, Right = Zero, Operator = EqualityOperator.Equal };
+
+        Invariant TestInvariant = new()
+        {
+            BooleanExpression = VariableEqualZero,
+            Text = VariableEqualZero.ToString(),
+        };
+
+        List<Invariant> InvariantList = new() { TestInvariant };
+
+        Verifier TestObject = new()
+        {
+            ClassName = ClassName,
+            FieldTable = TestFieldTable.ToReadOnly(),
+            MethodTable = ReadOnlyMethodTable.Empty,
+            InvariantList = InvariantList,
+            MaxDepth = 0,
+        };
+
+        return TestObject;
+    }
+}
