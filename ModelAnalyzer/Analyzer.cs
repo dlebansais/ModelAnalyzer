d97784665991bdabc5b3e18447d7207ded0e40da
dlebansais <dlebansais@gmail.com>
2023-01-07 18:48:13 +0100

    Cleanup.

--- ModelAnalyzer/Analyzer.cs
+++ ModelAnalyzer/Analyzer.cs
@@ -27,18 +27,7 @@ public abstract class Analyzer : DiagnosticAnalyzer
     {
         context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
         context.EnableConcurrentExecution();
-
-        switch (DiagnosticKind)
-        {
-            case SyntaxKind.ClassDeclaration:
-                context.RegisterSyntaxNodeAction(AnalyzeClassDeclaration, DiagnosticKind);
-                break;
-            case SyntaxKind.CompilationUnit:
-                context.RegisterSyntaxNodeAction(AnalyzeCompilationUnit, DiagnosticKind);
-                break;
-            default:
-                throw new NotImplementedException();
-        }
+        context.RegisterSyntaxNodeAction(AnalyzeClassDeclaration, DiagnosticKind);
     }
 
     private void AnalyzeClassDeclaration(SyntaxNodeAnalysisContext context)
@@ -62,43 +51,6 @@ public abstract class Analyzer : DiagnosticAnalyzer
         {
             Logger.LogException(exception);
         }
-    }
-
-    private void AnalyzeCompilationUnit(SyntaxNodeAnalysisContext context)
-    {
-        try
-        {
-            CompilationUnitSyntax CompilationUnit = (CompilationUnitSyntax)context.Node;
-            List<string> ExistingClassList = new();
-            List<ClassDeclarationSyntax> ClassDeclarationList = new();
-
-            foreach (MemberDeclarationSyntax Member in CompilationUnit.Members)
-                if (Member is FileScopedNamespaceDeclarationSyntax FileScopedNamespaceDeclaration)
-                    AnalyzeNamespaceMembers(context, FileScopedNamespaceDeclaration.Members, ExistingClassList, ClassDeclarationList);
-                else if (Member is NamespaceDeclarationSyntax NamespaceDeclaration)
-                    AnalyzeNamespaceMembers(context, NamespaceDeclaration.Members, ExistingClassList, ClassDeclarationList);
-                else if (Member is ClassDeclarationSyntax ClassDeclaration)
-                    AddClassDeclaration(ExistingClassList, ClassDeclarationList, ClassDeclaration);
-
-            AnalyzeClasses(context, CompilationUnit, ClassDeclarationList);
-
-            Manager.RemoveMissingClasses(ExistingClassList);
-        }
-        catch (OperationCanceledException)
-        {
-            throw;
-        }
-        catch (Exception exception)
-        {
-            Logger.LogException(exception);
-        }
-    }
-
-    private void AnalyzeNamespaceMembers(SyntaxNodeAnalysisContext context, SyntaxList<MemberDeclarationSyntax> members, List<string> existingClassList, List<ClassDeclarationSyntax> classDeclarationList)
-    {
-        foreach (MemberDeclarationSyntax NamespaceMember in members)
-            if (NamespaceMember is ClassDeclarationSyntax ClassDeclaration)
-                AddClassDeclaration(existingClassList, classDeclarationList, ClassDeclaration);
     }
 
     private void AddClassDeclaration(List<string> existingClassList, List<ClassDeclarationSyntax> classDeclarationList, ClassDeclarationSyntax classDeclaration)
